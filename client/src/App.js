1beac16abb2acd7fe35ddfd320a9ee802833da60
MinSungKim <timkim01@naver.com>
2022-09-13 16:17:45 +0900

    fix : client 초기화

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
-import Todo from './Todo'
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
-// 함수형 컴포넌트
 function App() {
-  const [state, setState] = useState({id:"id123", title: "제목 1", done: true});
-  
-  return(
+  return (
     <div className="App">
-      <Todo item={state}/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
