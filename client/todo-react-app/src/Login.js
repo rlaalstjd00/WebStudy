815c09b149e127251c9c5a3462b44cddd94dea2f
MinSungKim <timkim01@naver.com>
2022-10-01 22:54:31 +0900

    feat : 프론트 Login.js

--- client/todo-react-app/src/Login.js
+++ client/todo-react-app/src/Login.js
@@ -1,10 +1,67 @@
 import React from "react";
+import { signin } from "./service/ApiService";
+import  Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import {Container} from "@material-ui/core";
 
 function Login(props){
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const username = data.get("username");
+        const password = data.get("password");
+
+        signin({username: username, password: password});
+    };
+
     return (
-        <p>로그인 페이지</p>
+        <Container component="main" maxWidth="xs" style={{ marginTop: "8%" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography component="h1" variant="h5">
+                로그인
+              </Typography>
+            </Grid>
+          </Grid>
+          <form noValidate onSubmit={handleSubmit}>
+            {" "}
+            {/* submit 버튼을 누르면 handleSubmit이 실행됨. */}
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="username"
+                  label="아이디"
+                  name="username"
+                  autoComplete="username"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="패스워드"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button type="submit" fullWidth variant="contained" color="primary">
+                  로그인
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Container>
     );
-}
+};
 
 export default Login;
--- client/todo-react-app/src/service/ApiService.js
+++ client/todo-react-app/src/service/ApiService.js
@@ -27,3 +27,11 @@ export function call(api, method, request){
           console.log(error);
     });
 }
+
+export function signin(userDTO){
+    return call("/auth/signin", "POST", userDTO)
+        .then((response) => {
+            console.log("response : ", response);
+            alert("로그인 토큰: " + response.token);
+        });
+}
